69e6e649b76897ce869a7e7750b1844d619e9034
alexsandropancracio <alexsandropancracioofficial@gmail.com>
2025-07-04 14:33:44 -0300

    feat: add basic loop exercises

---
+++ exercises/loops/multiplication_table.py
@@ -0,0 +1,6 @@
+num = 2
+
+for z in range(1, 11):
+    total = num * z
+    print(z, 'x', num, '=', total)
+
---
+++ exercises/loops/sum_numbers_from_1_to_100.py
@@ -0,0 +1,6 @@
+num = 0
+
+for x in range(1, 101, 1):
+    num += x
+    
+print('The sum of the numbers from 1 to 100 is:', num)
